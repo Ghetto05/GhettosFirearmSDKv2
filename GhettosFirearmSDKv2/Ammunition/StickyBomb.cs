aca253844aa4d3d18e50eea7d0c0e65ec489e82f
Leon <beckerleon250@gmail.com>
2022-10-21 21:01:18 +0200

    Added sticky bomb script

---
+++ GhettosFirearmSDKv2/Ammunition/StickyBomb.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using ThunderRoad;
+
+namespace GhettosFirearmSDKv2
+{
+    [AddComponentMenu("Firearm SDK v2/Ammunition/Sticky bomb")]
+    [RequireComponent(typeof(Item))]
+    public class StickyBomb : MonoBehaviour
+    {
+        public List<Collider> colliders;
+        public Item item;
+
+        public void Awake()
+        {
+            item = GetComponent<Item>();
+            item.mainCollisionHandler.OnCollisionStartEvent += MainCollisionHandler_OnCollisionStartEvent;
+        }
+
+        private void MainCollisionHandler_OnCollisionStartEvent(CollisionInstance collisionInstance)
+        {
+            if (colliders.Count > 0)
+            {
+                if (colliders.Contains(collisionInstance.sourceCollider))
+                {
+                    StickTo(collisionInstance.targetCollider.attachedRigidbody);
+                }
+            }
+            else
+            {
+                StickTo(collisionInstance.targetCollider.attachedRigidbody);
+            }
+        }
+
+        public void StickTo(Rigidbody rb)
+        {
+            FixedJoint joint = item.gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = rb;
+        }
+    }
+}
