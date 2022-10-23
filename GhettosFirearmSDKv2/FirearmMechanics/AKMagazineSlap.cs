16d1305cdc5ff01bba198c322c87c9ce4efef585
Leon <beckerleon250@gmail.com>
2022-10-23 23:50:51 +0200

    Added AK magazine slap

---
+++ GhettosFirearmSDKv2/FirearmMechanics/AKMagazineSlap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ThunderRoad;
+using UnityEngine;
+
+namespace GhettosFirearmSDKv2.FirearmMechanics
+{
+    public class AKMagazineSlap : MonoBehaviour
+    {
+        public Firearm firearm;
+        public List<Collider> triggers;
+
+        private void Awake()
+        {
+            firearm.OnCollisionEvent += Firearm_OnCollisionEvent;
+        }
+
+        private void Firearm_OnCollisionEvent(Collision collision)
+        {
+            if (triggers.Contains(collision.contacts[0].thisCollider) && collision.gameObject.GetComponent<Magazine>() is Magazine mag)
+            {
+                if (mag != firearm.magazineWell.currentMagazine)
+                {
+                    firearm.magazineWell.Eject();
+                }
+            }
+        }
+    }
+}
