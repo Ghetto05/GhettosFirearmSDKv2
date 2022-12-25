274f0299158b54b7e39d72b3828887762561f8c1
Leon <beckerleon250@gmail.com>
2022-12-25 14:47:00 +0100

    Added additional fire sounds

---
+++ GhettosFirearmSDKv2/Util/AdditionalFireSoundManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GhettosFirearmSDKv2
+{
+    public class AdditionalFireSoundManager : MonoBehaviour
+    {
+        public BoltBase bolt;
+        public List<AudioSource> sounds;
+
+        private void Start()
+        {
+            bolt.OnFireEvent += Bolt_OnFireEvent;
+        }
+
+        private void Bolt_OnFireEvent()
+        {
+            Util.PlayRandomAudioSource(sounds);
+        }
+    }
+}
