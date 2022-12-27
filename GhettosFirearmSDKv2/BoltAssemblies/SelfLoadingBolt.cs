6706ada2bc413ebc538edf241fa6766acaa91d65
Leon Becker <81816604+Ghetto05@users.noreply.github.com>
2022-12-27 19:03:18 +0100

    Update SelfLoadingBolt.cs

--- GhettosFirearmSDKv2/BoltAssemblies/SelfLoadingBolt.cs
+++ GhettosFirearmSDKv2/BoltAssemblies/SelfLoadingBolt.cs
@@ -11,6 +11,9 @@ public override TryFire()
 {
 Cartridge car = firearm.magazineWell.TryLoadRound();
 if (car == null) return;
-
+FireMethods.Fire(firearm.item, firearm.actualHitscanMuzzle, car.data, out List<Vector3> hitpoints, out List<Vector3> trajectories)
+firearm.PlayFireSound();
+firearm.ApplyRecoil();
+car.Fire(hitpoints, trajectories, firearm.actualHitscanMuzzle);
 }
 }
