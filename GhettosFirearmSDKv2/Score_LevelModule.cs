a3f3f52f9fea986be77857df5d4c54d37ab9a903
Leon <beckerleon250@gmail.com>
2023-01-07 22:17:19 +0100

    Added new score module

---
+++ GhettosFirearmSDKv2/Score_LevelModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using ThunderRoad;
+
+namespace GhettosFirearmSDKv2
+{
+    public class Score_LevelModule : LevelModule
+    {
+        public float accuracy;
+        public int shotsFired;
+        public int shotsHit;
+        public int headshots;
+
+        public void CalculateAccuracy()
+        {
+            accuracy = (shotsHit / shotsFired) * 100f;
+        }
+    }
+}
