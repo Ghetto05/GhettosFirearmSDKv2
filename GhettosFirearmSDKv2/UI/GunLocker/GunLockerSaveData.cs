756d31174c60348c51240c0c23a729920e15554f
Leon <beckerleon250@gmail.com>
2023-01-07 22:19:39 +0100

    Save data of category "Prebuilts" now generates an item

--- GhettosFirearmSDKv2/UI/GunLocker/GunLockerSaveData.cs
+++ GhettosFirearmSDKv2/UI/GunLocker/GunLockerSaveData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using ThunderRoad;
 using GhettosFirearmSDKv2.SaveData;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace GhettosFirearmSDKv2
 {
@@ -21,6 +23,28 @@ namespace GhettosFirearmSDKv2
                 if (!list.Contains(data.category) && !data.category.Equals("Prebuilts")) list.Add(data.category);
             }
             return list;
+        }
+
+        public override void OnCatalogRefresh()
+        {
+            if (category.Equals("Prebuilts"))
+            {
+                Settings_LevelModule.GenerateGunSaveFolders();
+                ItemData itemData = (ItemData)Catalog.GetData<ItemData>(itemId).Clone();
+                itemData.modules = itemData.modules.CloneJson();
+                itemData.id = "Item" + id;
+                itemData.description = "Prebuilt version of the " + itemData.displayName + ".";
+                itemData.displayName = displayName;
+                PrebuiltLoader loader = new PrebuiltLoader();
+                loader.originalId = itemId;
+                loader.prebuiltId = id;
+                loader.itemData = itemData;
+                itemData.modules.Add(loader);
+                itemData.category = "Firearm Prebuilts";
+                itemData.iconAddress = id + ".Icon";
+                Catalog.LoadJson(itemData, JsonConvert.SerializeObject(itemData, Catalog.jsonSerializerSettings), FileManager.GetFullPath(FileManager.Type.JSONCatalog, FileManager.Source.Mods, "GhettosFirearmSDKv2Saves\\Saves\\" + "Item" + id + ".json"), "GhettosFirearmSDKv2_Saves");
+            }
+            base.OnCatalogRefresh();
         }
     }
 }
---
+++ GhettosFirearmSDKv2/UI/GunLocker/PrebuiltLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using ThunderRoad;
+using GhettosFirearmSDKv2.SaveData;
+
+namespace GhettosFirearmSDKv2
+{
+    public class PrebuiltLoader : ItemModule
+    {
+        public string prebuiltId;
+        public string originalId;
+
+        public override void OnItemLoaded(Item item)
+        {
+            if (item.contentCustomData == null || item.contentCustomData.Count == 0) item.contentCustomData = Catalog.GetData<GunLockerSaveData>(prebuiltId).dataList.CloneJson();
+            base.OnItemLoaded(item);
+        }
+    }
+}
