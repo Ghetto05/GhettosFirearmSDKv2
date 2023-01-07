2754a59a8c9b3bb2f7586f8e927e2ac562856600
Leon <beckerleon250@gmail.com>
2023-01-07 22:17:01 +0100

    Improved score

--- GhettosFirearmSDKv2/Settings_LevelModule.cs
+++ GhettosFirearmSDKv2/Settings_LevelModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Linq;
 using UnityEngine;
 using ThunderRoad;
 using System.Collections;
@@ -6,25 +7,16 @@ using Newtonsoft.Json;
 using System.IO;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using IngameDebugConsole;
 
 namespace GhettosFirearmSDKv2
 {
     public class Settings_LevelModule : LevelModule
     {
         public static Settings_LevelModule local;
-        public bool incapitateOnTorsoShot;
-        public bool infiniteAmmo;
-        public bool doCaliberChecks;
-        public bool doMagazineTypeChecks;
-        public float damageMultiplier;
-        public bool magazinesHaveNoCollision = true;
-        public float scopeX1MagnificationFOV = 28.5f;
+        public static Score_LevelModule localScore;
 
-        public int shotsFired = 0;
-        public int shotsHit = 0;
-        public int headshots = 0;
-        public float accuracy = 0f;
-
+        #region Initialization
         public override IEnumerator OnLoadCoroutine()
         {
             local = this;
@@ -39,66 +31,60 @@ namespace GhettosFirearmSDKv2
 
             EventManager.OnPlayerSpawned += EventManager_OnPlayerSpawned;
             EventManager.onCreatureSpawn += EventManager_onCreatureSpawn;
+            DebugLogConsole.AddCommand("SaveFirearmSettings", "Overrides the current Level_Master.json file", SendUpdate);
             return base.OnLoadCoroutine();
         }
 
-        private void SpawnGunLocker()
+        IEnumerator LoadScoreModule()
         {
-            Level.current.StartCoroutine(DelayedLockerSpawn());
-        }
-
-        private IEnumerator DelayedLockerSpawn()
-        {
-            yield return new WaitForSeconds(3f);
-            Vector3 position = new Vector3(41.3f, 2.5f, -43.0f);
-            Vector3 rotation = new Vector3(0, 120, 0);
-            Addressables.InstantiateAsync("GunLocker_Ghetto05_FirearmSDKv2", position, Quaternion.Euler(rotation.x, rotation.y, rotation.z), null, false).Completed += (System.Action<AsyncOperationHandle<GameObject>>)(handle =>
-            {
-                if (handle.Status == AsyncOperationStatus.Succeeded)
-                {
-                }
-                else
-                {
-                    Debug.LogWarning((object)("Unable to instantiate gun locker!"));
-                    Addressables.ReleaseInstance(handle);
-                }
-            });
+            Score_LevelModule score = new Score_LevelModule();
+            localScore = score;
+            level.mode.modules.Add(score);
+            score.level = level;
+            yield return score.OnLoadCoroutine();
         }
 
         private void EventManager_onCreatureSpawn(Creature creature)
         {
-            if (creature.isPlayer) return;
-            creature.gameObject.AddComponent<Chemicals.NPCChemicalsModule>();
-        }
+            //Chemicals (NPC)
+            if (!creature.isPlayer) creature.gameObject.AddComponent<Chemicals.NPCChemicalsModule>();
 
-        public void CalculateAccuracy()
-        {
-            accuracy = (shotsHit / shotsFired) * 100f;
+            //Score
+            if (localScore == null)
+            {
+                GameManager.local.StartCoroutine(LoadScoreModule());
+            }
         }
 
         private void EventManager_OnPlayerSpawned()
         {
-            shotsFired = 0;
-            shotsHit = 0;
-            headshots = 0;
-            accuracy = 0f;
-
+            //Chemicals (Player)
             Player.local.gameObject.AddComponent<Chemicals.PlayerEffectsAndChemicalsModule>();
 
+            //Penetration Levels
             foreach (RequiredPenetrationPowerData rppd in Catalog.GetDataList<RequiredPenetrationPowerData>())
             {
                 rppd.Init();
             }
 
+            //Gun locker
             if (Level.current.data.id.Equals("Home")) SpawnGunLocker();
         }
+        #endregion Initialization
 
-        public void SendUpdate()
-        {
-            string s = JsonConvert.SerializeObject(this, Catalog.jsonSerializerSettings);
-            OverrideJson(s);
-            OnValueChangedEvent?.Invoke();
-        }
+        #region Settings
+        #region Values
+        public bool incapitateOnTorsoShot;
+        public bool infiniteAmmo;
+        public bool doCaliberChecks;
+        public bool doMagazineTypeChecks;
+        public float damageMultiplier;
+        public bool magazinesHaveNoCollision = true;
+        public float scopeX1MagnificationFOV = 28.5f;
+
+        public float cartridgeEjectionTorque = 1f;
+        public float cartridgeEjectionForceRandomizationDevision = 3f;
+        #endregion Values
 
         private void OverrideJson(string content)
         {
@@ -128,7 +114,51 @@ namespace GhettosFirearmSDKv2
             File.WriteAllText(FileManager.GetFullPath(FileManager.Type.JSONCatalog, FileManager.Source.Mods, jsonName), builder.ToString());
         }
 
+        public void SendUpdate()
+        {
+            string s = JsonConvert.SerializeObject(this, Catalog.jsonSerializerSettings);
+            OverrideJson(s);
+            OnValueChangedEvent?.Invoke();
+        }
+
         public delegate void OnValueChangedDelegate();
         public static event OnValueChangedDelegate OnValueChangedEvent;
+        #endregion Settings
+
+        #region Gun Locker
+        private void SpawnGunLocker()
+        {
+            Level.current.StartCoroutine(DelayedLockerSpawn());
+        }
+
+        private IEnumerator DelayedLockerSpawn()
+        {
+            yield return new WaitForSeconds(3f);
+            Vector3 position = new Vector3(41.3f, 2.5f, -43.0f);
+            Vector3 rotation = new Vector3(0, 120, 0);
+            Addressables.InstantiateAsync("GunLocker_Ghetto05_FirearmSDKv2", position, Quaternion.Euler(rotation.x, rotation.y, rotation.z), null, false).Completed += (System.Action<AsyncOperationHandle<GameObject>>)(handle =>
+            {
+                if (handle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    Debug.LogWarning((object)("Unable to instantiate gun locker!"));
+                    Addressables.ReleaseInstance(handle);
+                }
+            });
+        }
+
+        public static void GenerateGunSaveFolders()
+        {
+            Directory.CreateDirectory(FileManager.GetFullPath(FileManager.Type.JSONCatalog, FileManager.Source.Mods, "GhettosFirearmSDKv2Saves\\Saves"));
+            ModData manifest = new ModData
+            {
+                Name = "GhettosFirearmSDKv2Saves",
+                Author = "Ghetto05",
+                ModVersion = "1.0",
+                Description = "Saves for your guns.",
+                GameVersion = GameSettings.instance.minModVersion
+            };
+            File.WriteAllText(FileManager.GetFullPath(FileManager.Type.JSONCatalog, FileManager.Source.Mods, "GhettosFirearmSDKv2Saves\\manifest.json"), JsonConvert.SerializeObject(manifest, Catalog.jsonSerializerSettings));
+        }
+        #endregion Gun Locker
     }
 }
