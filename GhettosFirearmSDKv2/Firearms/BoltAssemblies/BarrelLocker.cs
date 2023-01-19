2eb4aff50f1d71df256ff28c05b0730c2e1357bf
Leon Becker <81816604+Ghetto05@users.noreply.github.com>
2023-01-19 15:13:07 +0100

    Added barrel locker/tilter and pistol handle switcher

---
+++ GhettosFirearmSDKv2/Firearms/BoltAssemblies/BarrelLocker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GhettosFirearmSDKv2
+{
+    [AddComponentMenu("Firearm SDK v2/Bolt assemblies/Barrel tilt/lock")]
+    public class BarrelLocker : MonoBehaviour
+    {
+        public BoltBase bolt;
+        public Transform barrel;
+        public Transform lockedParent;
+        public Transform openedParent;
+        public BoltBase.BoltState lockedState;
+
+        private void FixedUpdate()
+        {
+            if (bolt.state == lockedState)
+            {
+                barrel.SetParent(lockedParent);
+            }
+            else
+            {
+                barrel.SetParent(openedParent);
+            }
+            barrel.localPosition = Vector3.zero;
+            barrel.localEulerAngles = Vector3.zero;
+        }
+    }
+}
