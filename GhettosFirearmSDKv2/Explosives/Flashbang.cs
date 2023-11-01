a2c7847b6d2b93bc17632c6f47781231e9ea51bf
Leon Becker <beckerleon250@gmail.com>
2023-11-01 23:49:40 +0100

    Fixed potential errors

--- GhettosFirearmSDKv2/Explosives/Flashbang.cs
+++ GhettosFirearmSDKv2/Explosives/Flashbang.cs
@@ -19,34 +19,41 @@ namespace GhettosFirearmSDKv2.Explosives
 
         public override void ActualDetonate()
         {
-            if (effect != null)
+            try
             {
-                effect.gameObject.transform.SetParent(null);
-                Player.local.StartCoroutine(delayedDestroy(effect.gameObject, effect.main.duration + 1f));
-                effect.Play();
-            }
-
-            if (!string.IsNullOrWhiteSpace(effectId)) Catalog.GetData<EffectData>(effectId).Spawn(transform).Play();
-            
-            Util.PlayRandomAudioSource(audioEffects);
-            Util.AlertAllCreaturesInRange(transform.position, 50);
-            foreach (AudioSource s in audioEffects)
-            {
-                s.gameObject.transform.SetParent(null);
-                Player.local.StartCoroutine(delayedDestroy(s.gameObject, s.clip.length + 1f));
-            }
-            
-            if (Vector3.Distance(transform.position, Player.local.head.cam.transform.position) < range && !Raycast(Player.currentCreature)) Chemicals.PlayerEffectsAndChemicalsModule.Flashbang(time);
-            
-            foreach (Creature cr in Creature.allActive)
-            {
-                Transform t = cr.animator.GetBoneTransform(HumanBodyBones.Head);
-                if (!cr.isPlayer && !cr.isKilled && Vector3.Distance(t.position, transform.position) < range && !Raycast(cr))
+                if (effect != null)
                 {
-                    cr.ragdoll.SetState(Ragdoll.State.Destabilized);
-                    StartCoroutine(FireMethods.TemporaryKnockout(time, 0, cr));
+                    effect.gameObject.transform.SetParent(null);
+                    Player.local.StartCoroutine(delayedDestroy(effect.gameObject, effect.main.duration + 1f));
+                    effect.Play();
+                }
+
+                item.physicBody.rigidBody.AddForce(Random.insideUnitSphere * 500);
+
+                if (!string.IsNullOrWhiteSpace(effectId)) Catalog.GetData<EffectData>(effectId).Spawn(transform).Play();
+
+                Util.PlayRandomAudioSource(audioEffects);
+                Util.AlertAllCreaturesInRange(transform.position, 50);
+                foreach (AudioSource s in audioEffects)
+                {
+                    s.gameObject.transform.SetParent(null);
+                    Player.local.StartCoroutine(delayedDestroy(s.gameObject, s.clip.length + 1f));
+                }
+
+                if (Vector3.Distance(transform.position, Player.local.head.cam.transform.position) < range && !Raycast(Player.currentCreature)) Chemicals.PlayerEffectsAndChemicalsModule.Flashbang(time);
+
+                foreach (Creature cr in Creature.allActive)
+                {
+                    Transform t = cr.animator.GetBoneTransform(HumanBodyBones.Head);
+                    if (!cr.isPlayer && !cr.isKilled && Vector3.Distance(t.position, transform.position) < range && !Raycast(cr))
+                    {
+                        cr.ragdoll.SetState(Ragdoll.State.Destabilized);
+                        StartCoroutine(FireMethods.TemporaryKnockout(time, 0, cr));
+                    }
                 }
             }
+            catch (System.Exception)
+            { }
             base.ActualDetonate();
         }
 
