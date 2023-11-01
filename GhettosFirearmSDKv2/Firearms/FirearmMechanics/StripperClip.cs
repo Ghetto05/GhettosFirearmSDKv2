047fe4023a2e325a0073144fcc9ac77f43d0d5f2
Leon Becker <beckerleon250@gmail.com>
2023-11-02 00:03:44 +0100

    Added WIP stripper clips

---
+++ GhettosFirearmSDKv2/Firearms/FirearmMechanics/StripperClip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using ThunderRoad;
+using System.Collections.Generic;
+
+namespace GhettosFirearmSDKv2
+{
+    public class StripperClip : MonoBehaviour
+    {
+        public Item item;
+        public Stack<Cartridge> cartridges;
+    }
+}
