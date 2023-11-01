f24f3651b7417582dbc3746a611e11bb00adb617
Leon Becker <beckerleon250@gmail.com>
2023-11-01 23:37:36 +0100

    Added disabling of despawn Fixed firearms despawning if gripped or kinematic, added spawn time delay and player spawn time delay to prevent errors

--- GhettosFirearmSDKv2/LevelModules/FirearmCleaner.cs
+++ GhettosFirearmSDKv2/LevelModules/FirearmCleaner.cs
@@ -9,11 +9,14 @@ namespace GhettosFirearmSDKv2
     {
         public override void ScriptUpdate()
         {
-            foreach (Firearm firearm in Firearm.all.ToArray())
+            if (FirearmsSettings.firearmDespawnTime > 0 && (Time.time - Player.local?.creature?.spawnTime > 30))
             {
-                if (firearm.item != null && !firearm.item.handlers.Any() && !firearm.item.tkHandlers.Any() && firearm.item.holder == null && Time.time - firearm.item.lastInteractionTime > FirearmsSettings.firearmDespawnTime)
+                foreach (Firearm firearm in Firearm.all.ToArray())
                 {
-                    firearm.item.Despawn();
+                    if (firearm.item != null && Time.time - firearm.item.spawnTime > 10f && !firearm.item.physicBody.rigidBody.isKinematic && !firearm.item.handlers.Any() && !firearm.item.tkHandlers.Any() && !firearm.item.isGripped && firearm.item.holder == null && Time.time - firearm.item.lastInteractionTime > FirearmsSettings.firearmDespawnTime)
+                    {
+                        firearm.item.Despawn();
+                    }
                 }
             }
         }
