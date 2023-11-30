260c6755634ea46ff26da91a17ffc0db3a64cff5
Leon Becker <beckerleon250@gmail.com>
2023-11-30 20:04:06 +0100

    Added chem lights

---
+++ GhettosFirearmSDKv2/Other/ChemLight.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ThunderRoad;
+using UnityEngine.Serialization;
+
+namespace GhettosFirearmSDKv2
+{
+    public class ChemLight : MonoBehaviour
+    {
+        public float burnTime = 300f;
+        public float lightUpTime = 10f;
+        public float lightDownTime = 30f;
+
+        public Item item;
+        public MeshRenderer[] renderers;
+        public Color color;
+        public float strength;
+        public AudioSource[] triggerSounds;
+        public float lightIntensity;
+        public Light[] lights;
+
+        private bool triggered;
+        private float triggerTime;
+        private bool appliedConstant;
+        private bool burntOut;
+
+        private void Start()
+        {
+            item.OnHeldActionEvent += (hand, handle, action) =>
+            {
+                if (action == Interactable.Action.UseStart)
+                    Trigger();
+            };
+        }
+
+        public void Trigger()
+        {
+            if (triggered) return;
+            triggered = true;
+            triggerTime = Time.time;
+            item.disallowDespawn = true;
+            Util.PlayRandomAudioSource(triggerSounds);
+        }
+
+        private void Update()
+        {
+            if (burntOut || !triggered)
+                return;
+            
+            float timePassed = Time.time - triggerTime;
+            float timeRemaining = triggerTime + burnTime - Time.time;
+
+            if (timePassed > burnTime)
+            {
+                ApplyLightLevel(0);
+                burntOut = true;
+                item.disallowDespawn = false;
+            }
+
+            if (timePassed <= lightUpTime)
+                ApplyLightLevel(timePassed / lightUpTime);
+            else if (timeRemaining <= lightDownTime)
+                ApplyLightLevel(timeRemaining / lightDownTime);
+            else if (!appliedConstant)
+            {
+                appliedConstant = true;
+                ApplyLightLevel(1f);
+            }
+
+            foreach (Light l in lights)
+            {
+                l.transform.position = transform.position + Vector3.up * 0.07f;
+            }
+        }
+
+        private void ApplyLightLevel(float level)
+        {
+            foreach (MeshRenderer r in renderers)
+            {
+                r.material.SetColor("_EmissionColor", color * strength * Mathf.Clamp01(level));
+            }
+
+            foreach (Light l in lights)
+            {
+                l.intensity = Mathf.Lerp(0, lightIntensity, Mathf.Clamp01(level));
+            }
+        }
+    }
+}
