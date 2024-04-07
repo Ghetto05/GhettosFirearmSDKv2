6a1c869310882ea775ee95342e76acd6381f56d0
Leon Becker <beckerleon250@gmail.com>
2024-04-07 03:23:45 +0200

    Add home adjustments

---
+++ GhettosFirearmSDKv2/LevelModules/HomeAdjustments.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Linq;
+using ThunderRoad;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace GhettosFirearmSDKv2
+{
+    public class HomeAdjustments : ThunderScript
+    {
+        public static HomeAdjustments local;
+        
+        public override void ScriptEnable()
+        {
+            local = this;
+            EventManager.OnPlayerPrefabSpawned += EventManagerOnOnPlayerPrefabSpawned;
+        }
+
+        private void EventManagerOnOnPlayerPrefabSpawned()
+        {
+            if (Level.current.data.id.Equals("Home"))
+                RemoveDrawers();
+        }
+
+        #region Gun Locker / Liam
+        
+        public GameObject workbenchAndLocker;
+        public GameObject liam;
+        
+        public void SpawnHomeItems()
+        {
+            if (FirearmsSettings.spawnWorkbenchAndLocker)
+                Level.current.StartCoroutine(DelayedLockerSpawn());
+            if (FirearmsSettings.spawnLiam)
+                Level.current.StartCoroutine(DelayedRigEditorSpawn());
+        }
+
+        private IEnumerator DelayedLockerSpawn()
+        {
+            yield return new WaitForSeconds(3f);
+            SpawnWorkbenchAndLocker();
+        }
+
+        private IEnumerator DelayedRigEditorSpawn()
+        {
+            yield return new WaitForSeconds(3f);
+            SpawnLiam();
+        }
+
+        public void SpawnLiam()
+        {
+            if (liam != null || !Level.current.data.id.Equals("Home"))
+                return;
+            Vector3 position = new Vector3(44.03f, 2.5f, -44.37f);
+            Vector3 rotation = new Vector3(0, -36, 0);
+            Addressables.InstantiateAsync("Ghetto05.Firearms.Clothes.Rigs.Editor", position, Quaternion.Euler(rotation.x, rotation.y, rotation.z), null, false).Completed += handle =>
+            {
+                if (handle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    Debug.LogWarning(("Unable to instantiate rig editor!"));
+                    Addressables.ReleaseInstance(handle);
+                }
+                liam = handle.Result;
+            };
+        }
+
+        public void SpawnWorkbenchAndLocker()
+        {
+            if (workbenchAndLocker != null || !Level.current.data.id.Equals("Home"))
+                return;
+            Vector3 position = new Vector3(41.3f, 2.5f, -43.0f);
+            Vector3 rotation = new Vector3(0, 120, 0);
+            Addressables.InstantiateAsync("Ghetto05.FirearmFrameworkV2.Locker", position, Quaternion.Euler(rotation.x, rotation.y, rotation.z), null, false).Completed += handle =>
+            {
+                if (handle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    Debug.LogWarning(("Unable to instantiate gun locker!"));
+                    Addressables.ReleaseInstance(handle);
+                }
+                workbenchAndLocker = handle.Result;
+            };
+        }
+
+        #endregion
+
+        #region Remove Drawers
+
+        private void RemoveDrawers()
+        {
+            Debug.Log(string.Join("\n", Level.current.baseTransform.GetComponentsInChildren<Handle>().Select(h => $"{h.gameObject.name} - {h.data.id}")));
+        }
+
+        #endregion
+    }
+}
