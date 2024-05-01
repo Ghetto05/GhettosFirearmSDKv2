a69d9694af3a94b2db23e19e21ab4e9158a9e15f
Leon Becker <beckerleon250@gmail.com>
2024-05-01 12:55:02 +0200

    Refactor

--- GhettosFirearmSDKv2/Firearms/FirearmMechanics/Hammer.cs
+++ GhettosFirearmSDKv2/Firearms/FirearmMechanics/Hammer.cs
@@ -27,29 +27,37 @@ namespace GhettosFirearmSDKv2
 
         public void InvokedStart()
         {
-            if (firearm == null && item.gameObject.TryGetComponent(out Firearm f)) firearm = f;
+            if (firearm == null && item.gameObject.TryGetComponent(out Firearm f))
+                firearm = f;
             firearm.OnFiremodeChangedEvent += Firearm_OnFiremodeChangedEvent;
             firearm.OnCockActionEvent += Firearm_OnCockActionEvent;
             hammerState = firearm.saveData.firearmNode.GetOrAddValue("HammerState", new SaveNodeValueBool());
-            if (hammerState.value) Cock(true, true);
-            else Fire(true, true);
+            if (hammerState.value)
+                Cock(true, true);
+            else
+                Fire(true, true);
         }
 
         private void Firearm_OnFiremodeChangedEvent()
         {
-            if (hasDecocker && firearm.fireMode == FirearmBase.FireModes.Safe) Fire();
+            if (hasDecocker && firearm.fireMode == FirearmBase.FireModes.Safe)
+                Fire();
         }
 
         private void Firearm_OnCockActionEvent()
         {
-            if (!allowManualCock || (!allowCockUncockWhenSafetyIsOn && firearm.fireMode == FirearmBase.FireModes.Safe)) return;
-            if (cocked) Fire(true);
-            else Cock();
+            if (!allowManualCock || (!allowCockUncockWhenSafetyIsOn && firearm.fireMode == FirearmBase.FireModes.Safe))
+                return;
+            if (cocked)
+                Fire(true);
+            else
+                Cock();
         }
 
         public void Cock(bool silent = false, bool forced = false)
         {
-            if (cocked && !forced) return;
+            if (cocked && !forced)
+                return;
             hammerState.value = true;
             cocked = true;
             if (hammer != null)
@@ -57,12 +65,14 @@ namespace GhettosFirearmSDKv2
                 hammer.localPosition = cockedPosition.localPosition;
                 hammer.localEulerAngles = cockedPosition.localEulerAngles;
             }
-            if (!silent) Util.PlayRandomAudioSource(cockSounds);
+            if (!silent)
+                Util.PlayRandomAudioSource(cockSounds);
         }
 
         public void Fire(bool silent = false, bool forced = false)
         {
-            if (!cocked && !forced) return;
+            if (!cocked && !forced)
+                return;
             hammerState.value = false;
             cocked = false;
             if (hammer != null)
@@ -70,7 +80,8 @@ namespace GhettosFirearmSDKv2
                 hammer.localPosition = idlePosition.localPosition;
                 hammer.localEulerAngles = idlePosition.localEulerAngles;
             }
-            if (!silent) Util.PlayRandomAudioSource(hitSounds);
+            if (!silent)
+                Util.PlayRandomAudioSource(hitSounds);
         }
     }
 }
