c86648ddbfa41fd0040a117794d8b1d15bfe7ccd
Leon Becker <beckerleon250@gmail.com>
2024-05-01 12:18:04 +0200

    Added magazine cleaning

--- GhettosFirearmSDKv2/LevelModules/FirearmCleaner.cs
+++ GhettosFirearmSDKv2/LevelModules/FirearmCleaner.cs
@@ -11,12 +11,19 @@ namespace GhettosFirearmSDKv2
         {
             if (FirearmsSettings.firearmDespawnTime > 0 && (Time.time - Player.local?.creature?.spawnTime > 30))
             {
-                foreach (Firearm firearm in Firearm.all.ToArray())
+                foreach (var firearmBase in FirearmBase.all.Where(x => x.GetType() == typeof(Firearm)).ToArray())
                 {
+                    var firearm = (Firearm)firearmBase;
                     if (firearm.item != null && Time.time - firearm.item.spawnTime > 10f && !firearm.item.physicBody.rigidBody.isKinematic && !firearm.item.handlers.Any() && !firearm.item.tkHandlers.Any() && !firearm.item.isGripped && firearm.item.holder == null && Time.time - firearm.item.lastInteractionTime > FirearmsSettings.firearmDespawnTime)
                     {
                         firearm.item.Despawn();
                     }
+                }
+
+                foreach (Magazine magazine in Magazine.all.Where(m => m.overrideItem == null).ToArray())
+                {
+                    if (magazine.item != null && Time.time - magazine.item.spawnTime > 10f && !magazine.item.physicBody.rigidBody.isKinematic && !magazine.item.handlers.Any() && !magazine.item.tkHandlers.Any() && !magazine.item.isGripped && magazine.item.holder == null && Time.time - magazine.item.lastInteractionTime > FirearmsSettings.firearmDespawnTime)
+                        magazine.item.Despawn();
                 }
             }
         }
