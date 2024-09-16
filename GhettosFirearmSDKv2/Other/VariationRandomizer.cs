f14a12c5aaa95db0add46d8e8d4c4a31dbed4c62
Leon Becker <beckerleon250@gmail.com>
2024-09-16 17:06:04 +0200

    Variation randomizer

---
+++ GhettosFirearmSDKv2/Other/VariationRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GhettosFirearmSDKv2
+{
+    public class VariationRandomizer : MonoBehaviour
+    {
+        public GameObject[] variants;
+
+        private void Start()
+        {
+            foreach (var v in variants)
+            {
+                v.SetActive(false);
+            }
+
+            variants[Random.Range(0, variants.Length)].SetActive(true);
+        }
+    }
+}
