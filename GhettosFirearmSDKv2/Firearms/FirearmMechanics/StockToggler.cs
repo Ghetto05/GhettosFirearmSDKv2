c4468a1a810a56406abd7ea0e4dbe021c426c0c6
LBecker <leon.becker@bikar.com>
2025-04-14 11:47:04 +0200

    Debug

--- GhettosFirearmSDKv2/Firearms/FirearmMechanics/StockToggler.cs
+++ GhettosFirearmSDKv2/Firearms/FirearmMechanics/StockToggler.cs
@@ -64,7 +64,7 @@ public class StockToggler : MonoBehaviour
 
     private void OnAction(IAttachmentManager.HeldActionData e)
     {
-        Debug.Log(e.ToString()); //TODO: remove debug
+        Debug.Log($"Stock save data exists: {_stockPosition != null}"); //TODO: remove debug
         if (e.Handle == toggleHandle && e.Action == Interactable.Action.UseStart)
         {
             if (currentIndex + 1 == positions.Length)
